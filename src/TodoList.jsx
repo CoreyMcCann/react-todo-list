14d732306805950e36516811a46f82fa3c990674
CoreyMcCann <coreymccann4@gmail.com>
2025-06-21 11:25:13 -0700

    adding functionality, still need to fix deletion

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,53 +1,57 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as React from 'react';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
+import TodoItem from './TodoItem';
+import TodoForm from './TodoForm';
 
-const initialTodos = [
-    {id: 1, text: 'Buy milk', completed: true}, 
-    {id: 2, text: 'Buy eggs', completed: false},
-    {id: 3, text: 'Buy bread', completed: false},
-]
+const getInitialData = () => {
+    const data = JSON.parse(localStorage.getItem("todos"));
+    if (!data) return [];
+    return data;
+};
 
 export default function TodoList() {
-    const [todos, setTodos] = useState(initialTodos);
+    const [todos, setTodos] = useState(getInitialData);
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    const removeTodo = (id) => {
+        setTodos(todos.filter(prevTodos => {
+            return prevTodos.filter((t) => t.id !== id);
+        }))
+    }
+
+    const toggleTodo = (id) => {
+        setTodos(prevTodos => {
+            return prevTodos.map(todo => {
+                if (todo.id === id) {
+                    return {...todo, completed: !todo.completed}
+                }
+                return todo;
+            })
+        })
+    }
+
+    const addTodo = (text) => {
+        setTodos(prevTodos => {
+            return [...prevTodos, {text: text, id: crypto.randomUUID(), completed: false}]
+        })
+    }
+
     return (
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            {todos.map(todo => {
-                const labelId = `checkbox-list-label-${todo.id}`;
-
-                return (
-                    <ListItem
-                        key={todo.id}
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="comments">
-                                <CommentIcon />
-                            </IconButton>
-                        }
-                        disablePadding
-                    >
-                        <ListItemButton role={undefined}  dense>
-                            <ListItemIcon>
-                                <Checkbox
-                                    edge="start"
-                                    checked={todo.completed}
-                                    tabIndex={-1}
-                                    disableRipple
-                                    inputProps={{ 'aria-labelledby': labelId }}
-                                />
-                            </ListItemIcon>
-                            <ListItemText id={labelId} primary={todo.text} />
-                        </ListItemButton>
-                    </ListItem>
-                );
-            })}
-        </List>
+            {todos.map(todo => (
+                <TodoItem 
+                    todo={todo} 
+                    key={todo.id} 
+                    remove={removeTodo} 
+                    toggle={toggleTodo}
+                />
+            ))}
+            <TodoForm addTodo={addTodo} />
+        </List> 
     );
 }
 
